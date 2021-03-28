8bb4b7fe09c6bc881f2096316074f95cf8954bd0
sinunu <52615838+sinunu@users.noreply.github.com>
2021-03-28 10:25:06 +0900

    Update 2 on GitHub branch Only

--- main.py
+++ main.py
@@ -22,3 +22,6 @@ print("Hello World Updates on GitHub")
 
 #adding new item in master branch
 print("Hello World Updates on Local Master Branch")
+
+#Updates on mainbranch of GitHub
+print("Hello World Updates 2 on GitHub Only")
